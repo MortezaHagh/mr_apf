a0368252ace09b86697cec942197953447186fdf
Morteza Haghbeigi <morteza.haghbeigi@unitedrobots.co>
2023-04-04 12:23:58 +0200

    start test v4

--- scripts/new_apf_init.py
+++ scripts/new_apf_init.py
@@ -21,7 +21,7 @@ class Run():
     def __init__(self):
 
         # # results
-        self.test_id = 15                              # check 2 !!!!!!!!!!
+        self.test_id = 1                              # check 
         self.test = "T" + str(self.test_id)
         # rospack = rospkg.RosPack()
         # pkg_path = "rospack.get_path('apf')"
